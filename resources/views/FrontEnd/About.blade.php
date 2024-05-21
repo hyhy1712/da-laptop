2eb5d3169ad5b4e70b3e9fcc846de128bac0be03
vietdh <vietdtds@gmail.com>
2024-05-21 23:59:35 +0700

    fe about foo hea

--- resources/views/FrontEnd/About.blade.php
+++ resources/views/FrontEnd/About.blade.php
@@ -1,5 +1,5 @@
 @extends('Layout')
-@section('title')    
+@section('title')
 {{ trans('home.about') }}
 @endsection
 @section('content-layout')
@@ -8,8 +8,8 @@
     <div class="container">
         <div class="breadcrumb">
             <ul class="d-flex align-items-center">
-                <li><a href="index.html">Home</a></li>
-                <li class="active"><a href="about.html">About Us</a></li>
+                <li><a href="index.html">{{trans('home.home')}}</a></li>
+                <li class="active"><a href="about.html">{{trans('home.about')}}</a></li>
             </ul>
         </div>
     </div>
@@ -22,16 +22,17 @@
         <div class="row">
             <div class="col-lg-6">
                 <div class="sidebar-img mb-all-30">
-                    <img src="{{asset('source/assets/frontend/img/blog/10.jpg')}}" alt="single-blog-img">
+                    <img src="{{asset('source/assets/frontend/img/blog/vietdh.jpg')}}" alt="single-blog-img">
                 </div>
             </div>
             <div class="col-lg-6">
                 <div class="about-desc">
-                    <h3 class="mb-10 about-title">About our success story</h3>
-                    <p class="mb-20">Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?</p>
-                    <p class="mb-20">Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo volup.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eu nisi ac mi malesuada vestibulum. Phasellus tempor nunc eleifend cursus molestie. Mauris lectus arcu, pellentesque at sodales sit amet, condimentum id nunc. Donec ornare mattis suscipit. Praesent fermentum accumsan vulputate.</p>
-                    <a href="#" class="return-customer-btn read-more">read more</a>
+                    <h3 class="mb-10 about-title">GIỚI THIỆU CHUNG</h3>
+                    <p class="mb-20">Công ty The11Laptop là một doanh nghiệp chuyên cung cấp các sản phẩm laptop chất lượng cao, phục vụ nhu cầu công nghệ của người dùng. The11Laptop do ông Doãn Huy Việt sở hữu và chính thức được thành lập vào ngày 1 tháng 1 năm 2024.</p>
+                    <p class="mb-20">The11Laptop cam kết mang đến cho khách hàng những dòng sản phẩm laptop đa dạng từ các thương hiệu nổi tiếng trên thế giới. Công ty không chỉ chú trọng đến chất lượng sản phẩm mà còn đặt khách hàng lên hàng đầu với dịch vụ hỗ trợ tận tình, chu đáo.</p>
+                    <p class="mb-20">Các sản phẩm của The11Laptop được chọn lọc kỹ càng để đảm bảo đáp ứng đầy đủ các tiêu chí về hiệu suất, độ bền và tính năng hiện đại. Bên cạnh việc cung cấp các dòng laptop mới, công ty còn cung cấp các dịch vụ bảo hành, sửa chữa và tư vấn kỹ thuật, nhằm đảm bảo rằng khách hàng sẽ luôn có được trải nghiệm tốt nhất khi sử dụng sản phẩm từ The11Laptop.</p>
+                    <p class="mb-20 a">Với sự lãnh đạo của ông Doãn Huy Việt, một người có tầm nhìn và kinh nghiệm trong lĩnh vực công nghệ, The11Laptop đang dần khẳng định vị thế của mình trên thị trường, trở thành địa chỉ tin cậy cho những ai đang tìm kiếm các sản phẩm laptop chất lượng.</p>
+
                 </div>
             </div>
         </div>
@@ -42,20 +43,20 @@
 <!-- About Us Team Start Here -->
 <div class="about-team pt-100 pt-sm-60">
     <div class="container">
-       <h3 class="mb-30 about-title">our exclusive team</h3>
+       <h3 class="mb-30 about-title">GIÁ TRỊ CỐT LÕI</h3>
         <div class="row text-center">
             <!-- Single Team Start Here -->
             <div class="col-lg-3 col-md-6 col-sm-6 col-6">
                 <div class="single-team mb-all-30">
                     <div class="team-img sidebar-img">
-                        <img src="{{asset('source/assets/frontend/img/team/2.jpg')}}" alt="team-image">
-                        <div class="team-link">
-                            <ul>
-                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                                <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-                            </ul>
-                        </div>
+                        <img src="{{asset('source/assets/frontend/img/team/01.png')}}" alt="team-image">
+{{--                        <div class="team-link">--}}
+{{--                           --}}{{-- <ul>--}}
+{{--                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>--}}
+{{--                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>--}}
+{{--                                <li><a href="#"><i class="fa fa-google-plus"></i></a></li>--}}
+{{--                            </ul>--}}
+{{--                        </div>--}}
                     </div>
                     <div class="team-info">
                         <h4>Marcos Alonso</h4>
